57b58089d8e709f8d97616acee215925f043693e
Emerson Leite <emerson.bl@gmail.com>
2024-03-05 19:57:14 -0300

    chore: send 'funções'

---
+++ 23 Funções/index2.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Config2 {
+    const PATH_TO_SQLITE_FILE = 'customers2.db';
+}
+
+function connection(): PDO
+{
+    return new PDO("sqlite:" . Config2::PATH_TO_SQLITE_FILE,"", "", [PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ]);
+}
+
+$db = connection();
+
+// Crieando tabela customers
+$db->exec("CREATE TABLE IF NOT EXISTS customers (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+  );");
+
+// inserindo pessoas
+// $db->exec("insert into customers(id, name, email) values(1, 'John Doe', 'john.doe@example.com') ");
+// $db->exec("insert into customers(id, name, email) values(2, 'Mary Adams', 'mary.ad@example.com') ");
+// $db->exec("insert into customers(id, name, email) values(3, 'Mary Adams', 'mary.ad@example.com') ");
+
+$query = $db->query("SELECT * FROM customers");
+$users = $query->fetchAll();
+
+// var_dump($users);
+
+foreach ($users as $user) {
+    echo $user->name . "<br>";
+}
