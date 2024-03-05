9770b466d93c389ce0ccb85bbbae6d22064371c4
Emerson Leite <emerson.bl@gmail.com>
2024-03-05 19:58:50 -0300

    chore: send 'switch'

---
+++ 12 Switch/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$name = "Sandra";
+
+switch ($name){
+    case "Emerson":
+        echo "É Emerson";
+        break;
+    case "Sandra":
+        echo "É Sandra";
+        break;
+    case "Stella":
+        echo "É Stella";
+        break;
+    default:
+        echo "Não é ninguém";
+}
+
+
+
+
+
