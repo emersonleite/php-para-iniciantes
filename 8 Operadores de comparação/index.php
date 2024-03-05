ce9f0eaf9225f8999208cbfb8f09c5dad9580ab6
Emerson Leite <emerson.bl@gmail.com>
2024-03-05 19:57:38 -0300

    chore: send 'operadores de comparação'

---
+++ 8 Operadores de comparação/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "../utils.php";
+
+// < , > , >=, <=, !=, ==, ===
+
+// !=, == compara valor
+//!==, === compara valor e tipo
+
+$result = 30 > 50;
+
+echo $result;
+
+// Para pegar o booleano
+
+var_dump($result);
+
